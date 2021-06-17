b605beb14095190b08d066a16cf96bc25f997a8b
karimulla06 <mdkarim1266@gmail.com>
2021-06-17 23:55:17 +0530

    added local state hook

--- src/BingoBoard.jsx
+++ src/BingoBoard.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
+import { useLocalState } from './useLocalState';
 import './style.css';
 
-const generateRandomArray = (size) => {
-  return [...Array(size+1).keys()]
-          .splice(1)
-          .sort(()=>0.5 - Math.random())
+const generateRandomArray = size => {
+  return [...Array(size + 1).keys()].splice(1).sort(() => 0.5 - Math.random());
 };
-export default function BingoBoard({size=5}) {
-  const randomArray = React.useMemo(()=>generateRandomArray(size*size),[size]);
-  const [selected,setSelected] = React.useState([]);
+export default function BingoBoard({ size = 5 }) {
+  const [randomArray, setRandomArray] = useLocalState('bingo', () =>
+    generateRandomArray(size * size)
+  );
 
-  const handleClick = (key) => {
-    setSelected(arr => [...arr,key]);
+  const [selected, setSelected] = useLocalState('bingo-selected', []);
+
+  const handleClick = key => {
+    setSelected(arr => [...arr, key]);
     console.log(key);
   };
 
   const getCell = key => {
     const cellProps = {
-      className:"bingo-box",
+      className: 'bingo-box',
       onClick: () => handleClick(key),
       style: {
         backgroundColor: selected.includes(key) && 'cyan'
       }
-    }
-    return(
-      <div {...cellProps} key={key} >{key}</div>
+    };
+    return (
+      <div {...cellProps} key={key}>
+        {key}
+      </div>
     );
-  }
+  };
   return (
     <>
-    <h2 className='title'>B I N G O</h2>
-    <div className="bingo-grid" style={{width:`${size*10}vw`}}>
-    {
-      randomArray.map(key => getCell(key))
-    }
-    </div>
+      <h2 className="title">B I N G O</h2>
+      <div className="bingo-grid" style={{ width: `${size * 10}vw` }}>
+        {randomArray.map(key => getCell(key))}
+      </div>
     </>
   );
 }
