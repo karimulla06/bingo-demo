fa865827cd86191f85705a989d6f757854a54ebe
karimulla06 <mdkarim1266@gmail.com>
2021-06-18 10:55:23 +0530

    createLinesObj

--- src/BingoBoard.jsx
+++ src/BingoBoard.jsx
@@ -6,53 +6,40 @@ const generateRandomArray = size => {
   return [...Array(size + 1).keys()].splice(1).sort(() => 0.5 - Math.random());
 };
 
-// const h = {
-//   0: [0, 1, 2, 3, 4],
-//   1: [5, 6, 7, 8, 9],
-//   2: [10, 11, 12, 13, 14],
-//   3: [15, 16, 17, 18, 19],
-//   4: [20, 21, 22, 23, 24]
-// };
-// const v = {
-//   0: [0, 5, 10, 15, 20],
-//   1: [1, 6, 11, 16, 21],
-//   2: [2, 7, 12, 17, 22],
-//   3: [3, 8, 13, 18, 23],
-//   4: [4, 9, 14, 19, 24]
-// };
-// const d = {
-//   0: [0, 6, 12, 18, 24],
-//   1: [4, 8, 12, 16, 20]
-// };
-const h = {
-  0: 5,
-  1: 5,
-  2: 5,
-  3: 5,
-  4: 5
-};
-const v = {
-  0: 5,
-  1: 5,
-  2: 5,
-  3: 5,
-  4: 5
-};
-const d = {
-  0: 5,
-  1: 5
+const createLinesObj = size => {
+  const obj = {
+    h: {},
+    v: {},
+    d: {
+      0: size,
+      1: size
+    }
+  };
+  for (let i = 0; i < size; i++) {
+    obj.h[i] = size;
+    obj.v[i] = size;
+  }
+  return obj;
 };
 
-const getLinesToCheck = (key, size) => {
-  const q = Math.floor(key / size);
-  const r = Math.floor(key % size);
-  console.log(key, h, v, d, q, r);
-  if (q === r) {
-    return [--h[q], --v[r], --d[0]];
-  } else if (q + r === size - 1) {
-    return [--h[q], --v[r], --d[1]];
+const getCompletedLines = (index, size, obj) => {
+  const { h, v, d } = obj;
+  const q = Math.floor(index / size);
+  const r = Math.floor(index % size);
+  const d1 = q == r;
+  const d2 = q + r === size - 1;
+  let arr = [];
+
+  if (d1 && d2) {
+    arr = [--h[q], --v[r], --d[0], --d[1]];
+  } else if (d1) {
+    arr = [--h[q], --v[r], --d[0]];
+  } else if (d2) {
+    arr = [--h[q], --v[r], --d[1]];
+  } else {
+    arr = [--h[q], --v[r]];
   }
-  return [--h[q], --v[r]];
+  return arr.filter(x => x === 0).length;
 };
 
 export default function BingoBoard({ size = 5 }) {
@@ -63,13 +50,16 @@ export default function BingoBoard({ size = 5 }) {
   const [selected, setSelected] = useLocalState('bingo-selected', []);
   const [canUndo, setCanUndo] = useLocalState('bingo-undo', false);
 
+  const [linesObj, setLinesObj] = useLocalState('bingo-lines', () =>
+    createLinesObj(size)
+  );
+
   const handleClick = (key, index) => {
     if (!selected.includes(key)) {
-      console.log(key, index);
       const newSelected = [...selected, key];
       setSelected(newSelected);
       setCanUndo(true);
-      console.log(getLinesToCheck(index, size));
+      console.log(getCompletedLines(index, size, linesObj));
     }
   };
   const handleUndo = () => {
@@ -80,12 +70,13 @@ export default function BingoBoard({ size = 5 }) {
     setRandomArray(generateRandomArray(size * size));
     setSelected([]);
     setCanUndo(false);
+    setLinesObj(createLinesObj(size));
   };
 
-  const getCell = key => {
+  const getCell = (key, index) => {
     const cellProps = {
       className: 'bingo-box',
-      onClick: () => handleClick(key),
+      onClick: () => handleClick(key, index),
       style: {
         backgroundColor: selected.includes(key) && 'cyan'
       }
